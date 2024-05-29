553d804307dfd0379a4b5bcaf7f7b4e0d4783836
JavFuertes <j.fuertesguadarrama@student.tudelft.nl>
2024-05-29 09:22:58 +0200

    Publish to GHio

--- bricks/tools/plots/em_plot.py
+++ bricks/tools/plots/em_plot.py
@@ -4,6 +4,11 @@ import numpy as np
 import matplotlib.cm as cm
 from ..utils import prepare_report
 
+from dash import Dash, dcc, html
+import plotly.graph_objects as go
+import numpy as np
+import matplotlib.cm as cm
+from ..utils import prepare_report
 
 def apply_opacity_to_colorscale(colorscale_name, opacity):
     cmap = cm.get_cmap(colorscale_name)
@@ -30,23 +35,53 @@ def EM_plot(report):
     cscale = apply_opacity_to_colorscale('RdYlGn_r', 0.75)
     for wall in walls:
         data_matrix, wall_param_labels, sources, description_annotations = prepare_report(report, wall)
-        heatmap = go.Heatmap(z=data_matrix,
-                                x=sources,
-                                y=wall_param_labels,
-                                colorscale= cscale,  
-                                zmin=0,
-                                zmax=5,
-                                colorbar=dict(title='Damage<br> Level'),
-                                hoverongaps=False,
-                                hoverinfo='text',
-                                text=np.vectorize(lambda desc: f"{desc}")(description_annotations),
-                                customdata=np.array(description_annotations))
+        heatmap = go.Heatmap(
+            z=data_matrix,
+            x=sources,
+            y=wall_param_labels,
+            colorscale=cscale,
+            zmin=0,
+            zmax=5,
+            colorbar=dict(
+                title='Damage Level:<br> ',
+                titleside='top',
+                tickmode='array',
+                tickvals=[0, 1, 2, 3, 4, 5],
+                ticktext=['0-None', '1-Negligible', '2-Moderate', '3-Severe', '4-Very Severe', '5-Extreme']
+            ),
+            hoverongaps=False,
+            hoverinfo='text',
+            text=np.vectorize(lambda desc: f"{desc}")(description_annotations),
+            customdata=np.array(description_annotations)
+        )
 
         layout = go.Layout(
-            title=f'{wall.capitalize()} empirical assessment',
-            xaxis=dict(title='Literature Source', side='bottom', showgrid=True),
-            yaxis=dict(title='SRI Parameter', showgrid=True, autorange='reversed'),
-            template='plotly_white'
+            #title=f'{wall.capitalize()} Empirical Assessment',
+            xaxis=dict(
+                title='Literature Source',
+                side='bottom',
+                showgrid=True,
+                gridcolor='lightgray'
+            ),
+            yaxis=dict(
+                title='SRI Parameter',
+                showgrid=True,
+                gridcolor='lightgray',
+                autorange='reversed'
+            ),
+            annotations=[
+                dict(
+                    xref='paper',
+                    yref='paper',
+                    x=0.5,
+                    y=-0.2,
+                    showarrow=False,
+                    text=f'Data Source: SRI assessment for {wall.capitalize()}',
+                    font=dict(size=12)
+                )
+            ],
+            template='plotly_white',
+            margin=dict(l=100, r=20, t=40, b=100)
         )
 
         fig = go.Figure(data=heatmap, layout=layout)
@@ -56,10 +91,13 @@ def EM_plot(report):
         'fontFamily': 'Arial, sans-serif',
         'color': '#3a4d6b'
     }
+
     tabs_content = [dcc.Tab(label=f"{wall.capitalize()}", children=[dcc.Graph(figure=fig)], style=tab_heading_style, selected_style=tab_heading_style) for wall, fig in zip(walls, figs)]
+
     app.layout = html.Div([
-        dcc.Tabs(children=tabs_content)
-    ])
+        dcc.Tabs(children=tabs_content)], style={'backgroundColor': 'white'})
+
     if __name__ == '__main__':
         app.run_server(debug=False)
+
     return app
--- bricks/tools/plots/ltsm_plot.py
+++ bricks/tools/plots/ltsm_plot.py
@@ -5,6 +5,7 @@ from plotly.subplots import make_subplots
 from plotly.colors import get_colorscale
 
 from ..utils import prepare_report, compute_param, get_color_from_scale
+from .em_plot import apply_opacity_to_colorscale
 
 def LTSM_plot(object):
     house = object.house
@@ -20,7 +21,6 @@ def LTSM_plot(object):
             figs = []
             wltsm = object.assessment['ltsm'][assessment]
             for i, wall in enumerate(house):
-                # ---------------------------------- Process --------------------------------- #
                 h = object.house[wall]['height']
                 int = object.process['int'][wall]
 
@@ -31,10 +31,9 @@ def LTSM_plot(object):
 
                 param = [ltsm_params, ltsm_values]
                 for dict_ in param:
-                    for key, value in dict_.items():
+                    for key, value in dict_.items(): # Unpacking all variables, not defined
                         globals()[key] = value
 
-                # ---------------------------------- Display --------------------------------- #
                 fig = make_subplots(rows=2, cols=1,
                                     vertical_spacing=0.1,
                                     shared_xaxes=True,
@@ -43,25 +42,30 @@ def LTSM_plot(object):
                                     y_title='Profile & Height [mm]',
                                     subplot_titles=('Relative Wall position', 'Subsidence profile'))
 
-                # -------------------- Custom measurements functionalities ------------------- #
                 if assessment == 'measurements':
                     z_measurement = int["z_lin"]
                     x_measurement = int["ax_rel"]
                     fig.add_trace(go.Scatter(x=x_measurement,
-                                            y= z_measurement,
+                                            y=z_measurement,
                                             mode='lines',
-                                            name='Subsidence profile'),
+                                            name='Subsidence profile', ),
                                 row=2, col=1)
                     
                     infl_list = object.soil['shape'][wall]['inflection_points']
                     for infl in infl_list:
-                        fig.add_trace(go.Scatter(x= [infl,infl], y=[0, h], mode='lines', name='Inflection point',
+                        fig.add_trace(go.Scatter(x=[infl, infl], y=[0, h], mode='lines', name='Inflection point',
                                                 line=dict(color='black', width=1, dash='dash')), row=1, col=1)
 
-                        fig.add_trace(go.Scatter(x= [infl,infl], y=[min(z_measurement), max(z_measurement)], mode='lines', name='Inflection point',
+                        fig.add_trace(go.Scatter(x=[infl, infl], y=[min(z_measurement), max(z_measurement)], mode='lines', name='Inflection point',
                                                 line=dict(color='black', width=1, dash='dash')), row=2, col=1)
+                    
+                    if z_measurement.max() > -5:
+                        fig.add_trace(go.Scatter(x=x,
+                                                y=np.full(len(x), limitline),
+                                                mode='lines', name=f'Limit Line [{limitline} mm]',
+                                                line=dict(color='black', dash='longdashdot', width=1)),
+                                    row=2, col=1)
 
-                # --------------------- Custom greenfield functionalities -------------------- #
                 if assessment == 'greenfield':
                     fig.add_trace(go.Scatter(x=x,
                                             y=w,
@@ -69,7 +73,6 @@ def LTSM_plot(object):
                                             name='Gaussian profile approximation'),
                                 row=2, col=1)
                     
-                    # Inflection traces
                     for z in [-xinflection, xinflection]:
                         fig.add_trace(go.Scatter(x=[z, z], y=[0, h], mode='lines', name='Inflection point',
                                                 line=dict(color='black', width=1, dash='dash')), row=1, col=1)
@@ -86,18 +89,16 @@ def LTSM_plot(object):
                     fig.add_trace(go.Scatter(x=x,
                                             y=np.full(len(x), limitline),
                                             mode='lines', name=f'Limit Line [{limitline} mm]',
-                                            line=dict(color='black', dash='dash', width=1)),
+                                            line=dict(color='black', dash='longdashdot', width=1)),
                                 row=2, col=1)
 
-                # -------------------------- Evaluation and building ------------------------- #
-                if i % 2 == 0:  # Wall is along the y axis
+                if i % 2 == 0:
                     xi = object.house[wall]['y'].max()
                     xj = object.house[wall]['y'].min()
                 else:
                     xi = object.house[wall]['x'].max()
                     xj = object.house[wall]['x'].min()
                 
-                # Plot wall shape
                 if object.assessment['ltsm'][assessment]['report']:
                     report = object.assessment['ltsm'][assessment]['report']
                     data_matrix, wall_param_labels, sources, description_annotations = prepare_report(report, wall)
@@ -107,20 +108,20 @@ def LTSM_plot(object):
                     
                     colorscale = 'RdYlGn_r'
                     colors = get_colorscale(colorscale)
-                    dlmax = 5 ## Find better way than hard coding
+                    dlmax = 5
                     color_matrix = [get_color_from_scale(damage, colors, dlmax) for damage in assessments]
                     segment_width = (xi - xj) / len(assessments)
 
-                    for i, (color, assess_i, comment,source) in enumerate(zip(color_matrix, assessments, comments,sources)):
+                    for i, (color, assess_i, comment, source) in enumerate(zip(color_matrix, assessments, comments, sources)):
                         fig.add_shape(
                             type="rect",
-                            x0= i * segment_width,
-                            y0= 0 ,
-                            x1= (i + 1) * segment_width,
+                            x0=i * segment_width,
+                            y0=0,
+                            x1=(i + 1) * segment_width,
                             y1=h,
-                            fillcolor= color[1],
+                            fillcolor=color[1],
                             opacity=0.7,
-                            line=dict(width=0),  # Remove borders if not needed
+                            line=dict(width=0),
                             row=1, col=1
                         )
                         fig.add_trace(go.Scatter(
@@ -132,57 +133,76 @@ def LTSM_plot(object):
                             marker=dict(size=0.1, color='rgba(0,0,0,0)'),
                             hoverinfo='text'
                         ))
-                    
+
                     fig.add_shape(type="rect",
                                     x0=0,
                                     y0=0,
                                     x1=xi - xj,
                                     y1=h,
-                                    fillcolor='rgba(49,50,51,0)',  # No fill color
-                                    line=dict(color='black', width=2),  # Black border with width 2
+                                    fillcolor='rgba(49,50,51,0)',
+                                    line=dict(color='black', width=2),
                                     row=1, col=1)
 
-                else:          
-                    try: # Use Boscardin & Cording (1989) if no report exists
+                else:
+                    try:
                         ecolor, cat = compute_param(strain)
-                    except: # If assessment has been tampered place grey
+                    except:
                         ecolor = 'rgba(49,50,51,0)'
                         cat = 'No assessment'
                     fig.add_shape(
-                    type="rect",
-                    x0= 0, y0=0,
-                    x1=xi - xj, y1=h,
-                    fillcolor=ecolor,
-                    row=1, col=1 )
+                        type="rect",
+                        x0=0, y0=0,
+                        x1=xi - xj, y1=h,
+                        fillcolor=ecolor,
+                        row=1, col=1)
                 
                 ind = np.argmax(assessments)
                 cat = comments[ind]
                 fig.update_layout(title=f'LTSM {wall} | e_tot = {strain:.2e} DL = {cat}',
                                 legend=dict(traceorder="normal"),
                                 template='plotly_white')
-                # Suppose 'condition' is your condition
 
                 if assessment == 'measurements':
                     valr2 = x_measurement.max() * 1.5 
                     x_range = [-valr2, valr2]
                     
-                    fig.update_xaxes(range= x_range, row=1, col=1)
-                    fig.update_xaxes(range= x_range, row=2, col=1)
+                    fig.update_xaxes(range=x_range, row=1, col=1)
+                    fig.update_xaxes(range=x_range, row=2, col=1)
 
-                # Remove duplicate legend labels
                 names = set()
                 fig.for_each_trace(
                     lambda trace:
                     trace.update(showlegend=False)
                     if (trace.name in names) else names.add(trace.name))
+                
                 figs.append(fig)
-        
-        except Exception as e: # Dont break individual plots
+
+                # Add color bar as a separate trace with specific height and position
+                fig.add_trace(go.Scatter(
+                    x=[None],
+                    y=[None],
+                    mode='markers',
+                    marker=dict(
+                        colorscale='RdYlGn',
+                        cmin=0,
+                        cmax=5,
+                        colorbar=dict(
+                            title="DL-Damage Level<br>(per assessment)",
+                            titleside="right",
+                            len=0.6,
+                            yanchor="bottom",
+                            y=0,
+                        )
+                    ),
+                    showlegend=False
+                ))
+
+        except Exception as e:
             print(f'{e} for assessment {assessment}')
             continue
 
-        wall_tabs = [dcc.Tab(label=f"Wall {i+1}", children=[dcc.Graph(figure=fig)], style= tab_style, selected_style= tab_style ) for i, fig in enumerate(figs)]
-        assessment_tab = dcc.Tab(label=f"{assessment.capitalize()} assessment", children=[dcc.Tabs(children=wall_tabs)], style=tab_style, selected_style= tab_style)
+        wall_tabs = [dcc.Tab(label=f"Wall {i+1}", children=[dcc.Graph(figure=fig)], style=tab_style, selected_style=tab_style) for i, fig in enumerate(figs)]
+        assessment_tab = dcc.Tab(label=f"{assessment.capitalize()} assessment", children=[dcc.Tabs(children=wall_tabs)], style=tab_style, selected_style=tab_style)
         all_assessment_tabs.append(assessment_tab)
 
     app.layout = html.Div([
@@ -195,184 +215,3 @@ def LTSM_plot(object):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def LTSM_plot(object):
-#     """
-#     Generates a Dash app with tabs for different LTSM assessments and wall plots.
-
-#     Parameters:
-#     object (Object): An object containing house data and LTSM assessment results.
-
-#     Returns:
-#     Dash app: A Dash app instance with the generated plots.
-#     """
-    
-#     def process_ltsm_results(wltsm, wall, assessment):
-#         """
-#         Process LTSM results for a given wall and assessment type.
-        
-#         Parameters:
-#         wltsm (dict): LTSM results for the given assessment.
-#         wall (str): Wall identifier.
-#         assessment (str): Type of assessment.
-        
-#         Returns:
-#         tuple: Processed parameters and values.
-#         """
-#         h = object.house[wall]['height']
-#         strain = wltsm['results'][wall]['e_tot']
-        
-#         if assessment == 'greenfield':
-#             ltsm_params = wltsm['variables'][wall]
-#             ltsm_values = wltsm['values'][wall]
-#         else:
-#             ltsm_params, ltsm_values = {}, {}
-
-#         return h, strain, ltsm_params, ltsm_values
-
-#     def create_plot(wall, h, strain, ltsm_params, ltsm_values, assessment):
-#         """
-#         Create a plotly figure for the given wall and assessment type.
-
-#         Parameters:
-#         wall (str): Wall identifier.
-#         h (float): Height of the wall.
-#         strain (float): Strain value.
-#         ltsm_params (dict): LTSM parameters.
-#         ltsm_values (dict): LTSM values.
-#         assessment (str): Type of assessment.
-
-#         Returns:
-#         go.Figure: A Plotly figure.
-#         """
-#         fig = make_subplots(
-#             rows=2, cols=1, vertical_spacing=0.1, shared_xaxes=True, 
-#             shared_yaxes=True, x_title='Length [m]', y_title='Height [m,mm]',
-#             subplot_titles=('Relative Wall position', 'Subsidence profile')
-#         )
-
-#         if assessment == 'measurements':
-#             int_data = object.process['int'][wall]
-#             fig.add_trace(go.Scatter(x=int_data['ax_rel'][::-1], y=int_data["z_lin"], mode='lines', name='Subsidence profile'), row=2, col=1)
-        
-#         elif assessment == 'greenfield':
-#             x = ltsm_params.get('x', [])
-#             w = ltsm_params.get('w', [])
-#             fig.add_trace(go.Scatter(x=x, y=w, mode='lines', name='Gaussian profile approximation'), row=2, col=1)
-
-#             add_inflection_and_influence_lines(fig, x, w, h, ltsm_values)
-
-#         return fig
-
-#     def add_inflection_and_influence_lines(fig, x, w, h, ltsm_values):
-#         """
-#         Add inflection and influence lines to the plot.
-
-#         Parameters:
-#         fig (go.Figure): The plotly figure to update.
-#         x (array): X values for the plot.
-#         w (array): W values for the plot.
-#         h (float): Height of the wall.
-#         ltsm_values (dict): LTSM values.
-#         """
-#         xinflection = ltsm_values.get('xinflection', 0)
-#         xlimit = ltsm_values.get('xlimit', 0)
-#         limitline = ltsm_values.get('limitline', 0)
-
-#         for z in [-xinflection, xinflection]:
-#             fig.add_trace(go.Scatter(x=[z, z], y=[0, h], mode='lines', name='Inflection point', line=dict(color='black', width=1, dash='dash')), row=1, col=1)
-#             fig.add_trace(go.Scatter(x=[z, z], y=[w.min(), w.max()], mode='lines', name='Inflection point', line=dict(color='black', width=1, dash='dash')), row=2, col=1)
-
-#         for influence in [-xlimit, xlimit]:
-#             fig.add_trace(go.Scatter(x=[influence, influence], y=[0, h], mode='lines', name='Influence area', line=dict(color='black', width=1, dash='dashdot')), row=1, col=1)
-#             fig.add_trace(go.Scatter(x=[influence, influence], y=[w.min(), w.max()], mode='lines', name='Influence area', line=dict(color='black', width=1, dash='dashdot')), row=2, col=1)
-
-#         fig.add_trace(go.Scatter(x=x, y=np.full(len(x), limitline), mode='lines', name=f'Limit Line [{limitline} mm]', line=dict(color='black', dash='dash', width=1)), row=2, col=1)
-
-#     def add_wall_shape(fig, wall, h, strain, assessment):
-#         """
-#         Add wall shape to the plot based on assessment.
-
-#         Parameters:
-#         fig (go.Figure): The plotly figure to update.
-#         wall (str): Wall identifier.
-#         h (float): Height of the wall.
-#         strain (float): Strain value.
-#         assessment (str): Type of assessment.
-#         """
-#         if assessment['report']:
-#             report = assessment['report']
-#             data_matrix, wall_param_labels, sources, description_annotations = prepare_report(report, wall)
-
-#             comments = description_annotations[0]
-#             assessments = data_matrix.flatten()
-
-#             colorscale = 'RdYlGn_r'
-#             colors = get_colorscale(colorscale)
-#             dlmax = 5
-#             color_matrix = [get_color_from_scale(damage, colors, dlmax) for damage in assessments]
-#             segment_width = (object.house[wall]['y'].max() - object.house[wall]['y'].min()) / len(assessments)
-
-#             for i, (color, assess_i, comment, source) in enumerate(zip(color_matrix, assessments, comments, sources)):
-#                 fig.add_shape(type="rect", x0=i * segment_width, y0=0, x1=(i + 1) * segment_width, y1=h, fillcolor=color[1], line=dict(width=0))
-#                 fig.add_trace(go.Scatter(x=[(i + 0.5) * segment_width], y=[h / 2], text=f"Assessment: {source}<br>DL: {assess_i}<br>Comment: {comment}", mode='markers', marker=dict(size=0.1, color='rgba(0,0,0,0)'), hoverinfo='text'))
-            
-#             ind = np.argmax(assessments)
-#             cat = comments[ind]
-#         else:
-#             ecolor, cat = compute_param(strain)
-#             fig.add_shape(type="rect", x0=0, y0=0, x1=object.house[wall]['y'].max() - object.house[wall]['y'].min(), y1=h, fillcolor=ecolor)
-        
-#         fig.update_layout(title=f'LTSM {wall} | e_tot = {strain:.2e} DL = {cat}', legend=dict(traceorder="normal"), template='plotly_white')
-#         remove_duplicate_legend_labels(fig)
-
-#     def remove_duplicate_legend_labels(fig):
-#         """
-#         Remove duplicate legend labels from the figure.
-
-#         Parameters:
-#         fig (go.Figure): The plotly figure to update.
-#         """
-#         names = set()
-#         fig.for_each_trace(lambda trace: trace.update(showlegend=False) if (trace.name in names) else names.add(trace.name))
-
-#     house = object.house
-#     app = Dash(__name__)
-#     assess_types = list(object.assessment['ltsm'].keys())
-#     all_assessment_tabs = []
-#     tab_style = {'fontFamily': 'Arial, sans-serif', 'color': '#3a4d6b'}
-
-#     for assessment in assess_types:
-#         try:
-#             figs = []
-#             wltsm = object.assessment['ltsm'][assessment]
-#             for wall in house:
-#                 h, strain, ltsm_params, ltsm_values = process_ltsm_results(wltsm, wall, assessment)
-#                 fig = create_plot(wall, h, strain, ltsm_params, ltsm_values, assessment)
-#                 add_wall_shape(fig, wall, h, strain, wltsm)
-#                 figs.append(fig)
-        
-#         except Exception as e:
-#             print(f'{e} for assessment {assessment}')
-#             continue
-
-#         wall_tabs = [dcc.Tab(label=f"Wall {i+1}", children=[dcc.Graph(figure=fig)], style=tab_style, selected_style=tab_style) for i, fig in enumerate(figs)]
-#         assessment_tab = dcc.Tab(label=f"{assessment.capitalize()} assessment", children=[dcc.Tabs(children=wall_tabs)], style=tab_style, selected_style=tab_style)
-#         all_assessment_tabs.append(assessment_tab)
-
-#     app.layout = html.Div([dcc.Tabs(children=all_assessment_tabs)])
-#     return app
